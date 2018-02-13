2928c633f5c3a2a95498b4ac6a3303342b5e1fd0
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-13 21:04:27 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author
+  attr_accessor :name, :post
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    @posts
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+end
